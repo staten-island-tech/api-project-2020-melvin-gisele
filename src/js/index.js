af79218d97cebe4eadeaef0e1e9b9a13bb478ec6
Melvin Aziz <58566746+MelvAziz@users.noreply.github.com>
2020-11-13 07:59:12 -0500

    commit

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,13 @@
 const DOMSelectors = {
   grid: document.querySelector(".comic-grid"),
-};
+  nextButton: document.querySelector(".next"),
+}
 const publickey = `bf4202f8db0e27501960cf60881777d4`;
 const hash = `59273d4a371e6ef2bd7d6a729260d486`;
-const character = `https://gateway.marvel.com:443/v1/public/characters?&ts=1&apikey=${publickey}&hash=${hash}&nameStartsWith=a`;
+const character = `https://gateway.marvel.com:443/v1/public/characters?&ts=1&apikey=${publickey}&hash=${hash}&nameStartsWith=a&offset=0`;
+
+
+const nextButton
 
 const defaultPage = async function() {
   try {
@@ -16,21 +20,20 @@ const defaultPage = async function() {
         "beforeend",
         `<div class="movie-card">
        <div class="comic-front">
-         <img
-           src="${comic.thumbnail.path}/portrait_medium.${comic.thumbnail.extension}"
-           alt=""
-           class="poster"
-         />
        </div>
-       <div class="comic-back">
+       <div class="comic">
          <h3 class="comic-header">${comic.name}</h3>
+         <img
+         src="${comic.thumbnail.path}/portrait_medium.${comic.thumbnail.extension}"
+         alt=""
+         class="poster"
+          />
          <div class="pages">
            <p class="comic-count">Number of Comics:${comic.comics.available}</p>
          </div>
 
          <div class="date">
-           <p class="modified">Modified:</p>
-           <p class="modified">${comic.modified}</p>
+           <p class="modified">Modified:${comic.modified}</p>
          </div>
 
          <div class="comic-description">
