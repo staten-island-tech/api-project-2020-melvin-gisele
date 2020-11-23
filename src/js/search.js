7ca162822cb6b438ef9a1ab9d36875eccebb33e5
giselebury <54081443+giselebury@users.noreply.github.com>
2020-11-23 12:17:27 -0500

    adf

--- src/js/search.js
+++ src/js/search.js
@@ -10,11 +10,12 @@ const listen = function () {
     DOMSelectors.grid.innerHTML = "";
     const searchParams = DOMSelectors.searchArea.value;
     console.log(searchParams)
-    const searchQuery = async function () { 
-      try{
+    const searchQuery = async function () {  
         const publickey = `bf4202f8db0e27501960cf60881777d4`;
         const hash = `59273d4a371e6ef2bd7d6a729260d486`;
         const character = `https://gateway.marvel.com:443/v1/public/characters?&ts=1&apikey=${publickey}&hash=${hash}&limit=30&nameStartsWith=${searchParams};`
+        console.log(character)
+      try{
         const response = await fetch(character);
         const data = await response.json();
         const char = data.data.results;
