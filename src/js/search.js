2fe20397ac037963fe88abf414390609f28b2c10
giselebury <54081443+giselebury@users.noreply.github.com>
2020-11-23 13:39:54 -0500

    fixed it

--- src/js/search.js
+++ src/js/search.js
@@ -13,7 +13,7 @@ const listen = function () {
     const searchQuery = async function () {  
         const publickey = `bf4202f8db0e27501960cf60881777d4`;
         const hash = `59273d4a371e6ef2bd7d6a729260d486`;
-        const character = `https://gateway.marvel.com:443/v1/public/characters?&ts=1&apikey=${publickey}&hash=${hash}&limit=30&nameStartsWith=${searchParams};`
+        const character = `https://gateway.marvel.com:443/v1/public/characters?&ts=1&apikey=${publickey}&hash=${hash}&limit=30&nameStartsWith=${searchParams}`
         console.log(character)
       try{
         const response = await fetch(character);
